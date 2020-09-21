723a5f0813030c3c68060526954ed52d0e08972f
brandononeal <baoneal95@gmail.com>
2020-09-21 13:16:43 -0700

    naming navbar

--- kickstarter-success/src/components/NavBar.js
+++ kickstarter-success/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function () {
+export default function NavBar() {
   return (
     <nav className="nav-bar">
       <Link to="/login">Login</Link>
