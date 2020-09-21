2bd0542f0fe4ddf26cf355c8c60f671c0234180e
brandononeal <baoneal95@gmail.com>
2020-09-21 16:58:38 -0700

    added schema and validation

--- kickstarter-success/src/components/LoginForm.js
+++ kickstarter-success/src/components/LoginForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import * as yup from "yup";
+import schema from "../validation/loginSchema";
 
 const initialFormValues = {
   username: "",
@@ -14,33 +16,72 @@ const initialFormErrors = {
 export default function LoginForm() {
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(true);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+  };
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
 
   return (
     <div className="login-form">
       <h2>Login</h2>
       <form>
-        <div className="errors-container"></div>
+        <div className="errors-container">
+          <p>{formErrors.username}</p>
+          <p>{formErrors.password}</p>
+        </div>
 
         <div className="form-container">
           <div className="input-container">
-            <lable>Username:</lable>
             <input
               type="text"
               name="username"
-              placeholder="Enter your username"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={change}
             />
           </div>
 
           <div className="input-container">
-            <lable>Password:</lable>
             <input
               type="password"
               name="password"
-              placeholder="Enter your password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={change}
             />
           </div>
 
-          <button>Submit</button>
+          <button disabled={disabled}>Submit</button>
 
           <Link to="/register">Register</Link>
         </div>
